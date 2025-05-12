e31b96d6c8c1eca2aaadb4e37e294deb18699c1a
Michael Wheeler <echopeakdev@gmail.com>
2025-05-12 17:44:48 -0400

    Fixed hard coded localhost port.

--- extension/src/ChromeBackgroundRuntime.ts
+++ extension/src/ChromeBackgroundRuntime.ts
@@ -2,12 +2,38 @@ import { createSocket, SocketInterface } from "./helpers/createSocket";
 export class ChromeBackgroundRuntime {
   private clientSocket?: SocketInterface;
   private ytTabs = new Set<number>();
+  private readonly nativeAppId = "com.ytmediacontroller.app";
   constructor(){
-
-      this.clientSocket = createSocket(`http://localhost:56546`);
-      this.clientSocket.on("PlayEvent", this.handlePlayEvent);
-    
+    this.init();
     chrome.runtime.onMessage.addListener(this.handleMessage);
+  }
+  private setupCLientSocket = (port: string) => {
+    this.clientSocket = createSocket(`http://localhost:${port}`);
+    this.clientSocket.on("PlayEvent", this.handlePlayEvent);
+    this.clientSocket.on("error", (error) => {
+      console.error("Socket error:", error);
+      this.clientSocket = undefined;
+    });
+    console.log("Client socket created");
+  }
+
+  private init = () => {
+    chrome.storage.local.get(["backendServerPort", "controlServerPort"], (result) => {
+      if(result){
+        const { backendServerPort } = result;
+        this.setupCLientSocket(backendServerPort);
+      }else{
+        this.sendNativeMessage({action: 'getSettings'}, (response)=> {
+          if(response){
+            this.setupCLientSocket(response.backendServerPort);
+            chrome.storage.local.set({ 
+              backendServerPort: response.backendServerPort,
+              controlServerPort: response.controlServerPort,
+            });
+          }
+        });
+      }
+    });
   }
 
   private handlePlayEvent = (data: any) => {
@@ -16,7 +42,7 @@ export class ChromeBackgroundRuntime {
   }
 
   private sendNativeMessage(data: Record<string, unknown>, callback: (response: any) => void) {
-    chrome.runtime.sendNativeMessage("com.ytmediacontroller.app", data, callback);
+    chrome.runtime.sendNativeMessage(this.nativeAppId, data, callback);
   }
 
   private handleMessage = (message: Record<string, string | number>, sender: chrome.runtime.MessageSender, sendResponse: (response?: any) => void) => {
@@ -42,22 +68,47 @@ export class ChromeBackgroundRuntime {
         break;
       }
       case "playbackEnded":{
-        this.clientSocket?.send({event: "playbackEnded", data: {
+        this.sendSocketMessage({event: "playbackEnded", data: {
           videoId: message.videoId
         }});
         break;
       }
       
       case "playbackStarted": {
-        this.clientSocket?.send({event: "playbackStated", data: {videoId: message.videoId}});
+        this.sendSocketMessage({event: "playbackStated", data: {videoId: message.videoId}});
         break;
       }
     }
   }
 
-  public sendSocketMessage = (data: Record<string, unknown>) => {
-    this.clientSocket?.send(data);
-  }
+  private waitForClientSocket = async (): Promise<SocketInterface | undefined> => {
+    const maxWaitTime = 30000;
+    const interval = 100;
+    let elapsedTime = 0;
+
+    return new Promise((resolve) => {
+      const checkSocket = () => {
+        if (this.clientSocket) {
+          resolve(this.clientSocket);
+        } else if (elapsedTime >= maxWaitTime) {
+          resolve(undefined);
+        } else {
+          elapsedTime += interval;
+          setTimeout(checkSocket, interval);
+        }
+      };
+      checkSocket();
+    });
+  };
+
+  public sendSocketMessage = async (data: Record<string, unknown>) => {
+    const socket = await this.waitForClientSocket();
+    if (socket) {
+      socket.send(data);
+    } else {
+      console.error("Client socket is not available after waiting for 30 seconds.");
+    }
+  };
 
   public sendMessage(data: Record<string, unknown>) {
     chrome.runtime.sendMessage(data);
